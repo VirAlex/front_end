1158683565b99c63b4be63b97f135b16d3dd705e
VirAlex <alexou73@gmail.com>
2020-02-18 15:44:39 +0100

    footer adressé

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,21 @@ import React from "react"
 import logo from "../images/echap_logo.jpg"
 import styles from "../css/footer.module.css"
 import icons from "../constants/SocialLinks"
+import { FaPhoneSquare,
+  FaAddressCard} from "react-icons/fa"
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.logo}>
-        <img src={logo} alt="john smilga logo" style={{width:90, borderRadius:"8px"}}></img>
+        <img src={logo} alt="Echapée Belle Logo" style={{width:90, borderRadius:"8px"}}></img>
+        <div className={styles.flex}>
+          <p>Echapée Belle</p>
+          <FaAddressCard style={{ marginLeft: "1em" }}/>
+          <p style={{ marginLeft: "0.2em" }}>725 Route Nationale 6 73190 Saint-Jeoire-Prieuré</p>
+          <FaPhoneSquare style={{marginLeft:"1em"}}/>
+          <p style={{ marginLeft: "0.2em" }}>0620258896</p>
+        </div>
       </div>
       <div className={styles.icons}>
         {icons.map(item => {
